a4eef5372f6113222dc5351b0264fb9b9a8f461f
Ed Tansey <office.fwd@yahoo.com>
2021-08-17 18:07:09 +0100

    Query multiple tables with JOIN

--- inc/functions.php
+++ inc/functions.php
@@ -13,6 +13,26 @@ function full_catalog_array() {
     return $catalog;
 }
 
+function single_item_array($id) {
+    include("connection.php");
+  
+    try {
+        $results = $db->query(
+            "SELECT title, category, img, format, year, genre, publisher, isbn
+            FROM Media
+            JOIN Genres ON Media.genre_id = Genres.genre_id
+            LEFT OUTER JOIN Books ON Media.media_id = Books.media_id WHERE Media.media_id = $id"
+        );
+    } catch (Exception $e) {
+        echo "Unable to retrieved results";
+        exit;
+    }
+  
+    $catalog = $results->fetch();
+    return $catalog;
+}
+var_dump(single_item_array(1));
+
 function get_item_html($id,$item) {
     $output = "<li><a href='details.php?id="
         . $id . "'><img src='" 
