822bd0f9f0bb399560650a90e151f5389052fac1
Ed Tansey <office.fwd@yahoo.com>
2021-08-17 13:49:59 +0100

    Return details of exception

--- inc/connection.php
+++ inc/connection.php
@@ -2,9 +2,10 @@
 
 try {
   $db = new PDO("sqlite:".__DIR__."/database.db");
-  var_dump($db);
+  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
   echo "Unable to connect";
+  echo $e->getMessage();
   exit;
 }
 
